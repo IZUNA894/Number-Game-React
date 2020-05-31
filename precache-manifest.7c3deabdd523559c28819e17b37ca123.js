56726fe6d11e6eb32065f706c94121ffc9991805
IZUNA894 <48329138+IZUNA894@users.noreply.github.com>
2020-05-31 21:45:05 +0530

    Updates

---
+++ precache-manifest.7c3deabdd523559c28819e17b37ca123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b19caa127ad4355a10419703f9230b5",
+    "url": "/Number-Game-React/index.html"
+  },
+  {
+    "revision": "afd11dcd918b420e3abb",
+    "url": "/Number-Game-React/static/css/main.661e324c.chunk.css"
+  },
+  {
+    "revision": "f05345fa3e3d8709d349",
+    "url": "/Number-Game-React/static/js/2.884bb6da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Number-Game-React/static/js/2.884bb6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd11dcd918b420e3abb",
+    "url": "/Number-Game-React/static/js/main.a06a2804.chunk.js"
+  },
+  {
+    "revision": "9d2996a7686f1c56d131",
+    "url": "/Number-Game-React/static/js/runtime-main.bd71d891.js"
+  }
+]);
